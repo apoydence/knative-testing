24809f3ff43c1eddb34b38e464786f3f6dfc84f1
Andrew Poydence <apoydence@pivotal.io>
2018-10-03 17:02:27 -0600

    Adds echo

---
+++ cmd/echo/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		io.Copy(w, r.Body)
+	}))
+}
